91f9697383f883915797b5246e21a4c5e65947b7
chris-pc <cvand15@student.sdu.dk>
2023-12-19 00:19:35 +0100

    added method for getting data class to work with graph class

--- main_project/graph_creator.py
+++ main_project/graph_creator.py
@@ -26,6 +26,86 @@ from time import sleep
 import os
 import sys
 
+@dataclass
+class Graphdata:
+    """Each instance of this class creates a dataclass that stores various information related to simulating the evolution of patches of land
+
+    Parameters
+    ----------
+    _land_patches: int, default = 0
+        Stores total number of landpatches on the graph (coresponds to total number of vertices)
+    _tree_patches: List[int], default = []
+        Stores current number of tree patches on the graph
+    _rock_patches: List[int], default = []
+        Stores current number of rock patches on the graph
+    _ignited_tree_patches: List[int], default = []
+        Stores current number of ignited tree patches
+    _consumed_tree_patches: int, default = 0
+        Stores total number of tree patches consumed by fire
+    _rock_to_tree_counter: int, default = 0
+        Stores number of rock patches that, during the course of simulation, have turned into tree patches
+    _firefighters: List[int] = []
+        Stores current number of firefighters
+    _dead_firefighters: int = 0
+        Stores number of firefighters, who have perished
+    """
+    _land_patches: int = 0
+    _tree_patches: List[int] = field(default_factory=list)
+    _rock_patches: List[int] = field(default_factory=list)
+    _ignited_tree_patches: List[int] = field(default_factory=list)
+    _consumed_tree_patches: int = 0
+    _rock_to_tree_counter: int = 0
+    _firefighters: List[int] = field(default_factory=list)
+    _dead_firefighters_counter: int = 0
+
+    def update_patches(self, patches_map: Dict[str, Type]) -> None:
+        """Updates number of tree patches, rock patches and forest fires"""
+
+        # Store variables for updating patches of instance
+        treepatches = self._tree_patches
+        rockpatches = self._rock_patches
+        forest_fires = self._ignited_tree_patches
+
+        # Initialize counters
+        treepatches_counter = 0
+        rockpatches_counter = 0
+        forest_fires_counter = 0
+
+        # Iterate over patches_map
+        for vertex, patch in patches_map.items():
+            # check if patch mapped to vertex is rock or tree
+            if isinstance(patch, lc.Rockpatch):
+                rockpatches_counter += 1
+            else:
+                treepatches_counter += 1
+                # check if tree patch is ignited
+                if patch.is_ignited:
+                    forest_fires_counter += 1
+        
+        # Append patch count to instance data
+        treepatches.append(treepatches_counter) 
+        rockpatches.append(rockpatches_counter)
+        forest_fires.append(forest_fires_counter) 
+
+    def update_rock_to_tree_counter(self) -> None:
+        """Updates number of rock patches that swapped to a tree patch"""
+        # TODO
+
+        self._rock_to_tree_counter += 1
+    
+    def update_firefighter_list(self) -> None:
+        """Updates the list of current alive fire fighters"""
+
+        # Compute number of currently alive firefighters
+        firefighters = self._firefighters[0] - self._dead_firefighters_counter
+
+        # Append total number of alive firefighters to firefighter list
+        self._firefighters.append(firefighters)
+
+    def update_dead_firefighters_counter(self):
+        """Updates counter of dead fire fighters"""
+        
+        self._dead_firefighters_counter += 1
 
 class Graph():
     """Each instance of this class creates a Graph with edges and vertices populated with landpatches (either rock or tree) and a coloring pattern. """
@@ -33,7 +113,8 @@ class Graph():
     def __init__(
         self,
         edges: list,
-        tree_distribution: float) -> None:
+        tree_distribution: float,
+        firefighters: int = 0) -> None:
         """
         Parameters
         ----------
@@ -41,6 +122,8 @@ class Graph():
             List containing the edges (Tuples of 2 vertices) forming the 2D surface for the graph.
         tree_distribution: float
             Percentage distribution of tree to rock patches ratio
+        firefighters: int, default = 0
+            firefighters at instance creation
         vertices_dict: Optional[dict], default = {}
             dictionary with vertices as key and values for each vertex: color, frustration, neighbours
         total_frustration: Optional[float], default = 0
@@ -53,15 +136,20 @@ class Graph():
         self._tree_distribution = tree_distribution
         self._vertices_list = self._create_vertices_list()
         self._vertices_neighbours = self._create_neighbour_dict()
-        self._patches_map = self._populate_patches()                # Map patch type to vertex
-        self._color_map = self._create_color_map()                  # Map color to vertex
-        self._firefighters_map = self._deploy_firefighters()        # Map firefighters to vertex
+        self._patches_map = self._populate_patches()                            # Map patch type to vertex
+        self._color_map = self._create_color_map()                              # Map color to vertex
+        self.firefighters = firefighters                       
+        self._firefighters_map = self._deploy_firefighters(firefighters)        # Map firefighters to vertex
         self._is_connected = False
 
         # visualize opening instance of graph
         self._vis_graph = vis_rfg.Visualiser(self._edges, vis_labels=True, node_size=200)
         self._vis_graph.update_node_colours(self._color_map)
         sleep(0.6) # add delay to show initial graph
+
+        # Create data class instance to store graph data
+        self._graph_data = Graphdata()
+        self._initialize_data()
 
     # class methods
     
@@ -198,6 +286,17 @@ class Graph():
                 firefighter_map[vertex] = lc.Firefighter()
         
         return firefighter_map
+    
+    def _initialize_data(self):
+        """Stores initital data from graph instance creation in dataclass"""
+
+        data = self._graph_data
+
+        data._land_patches = [len(self._vertices_list)]
+        data._tree_patches = [round(data._land_patches[0] * (self._tree_distribution / 100.0))]
+        data._rock_patches = [data._tree_patches[0] - data._tree_patches[0]]
+        data._firefighters = [self.firefighters]
+
         
     def __eq__(self, other):
         """Return true if edges of this instance is equal to edges of other instance of same class"""
@@ -216,91 +315,13 @@ class Graph():
         """Return a Python-like representation of this this instance"""
         return f"GraphCreater({self._edges}, {self._color_pattern})"
 
-test_graph = Graph([(1, 2), (1,3), (2,3)], tree_distribution=66)
+test_graph = Graph([(1, 2), (1,3), (2,3)], tree_distribution=66, firefighters=2)
 print(test_graph._patches_map)
 print("graph: ", test_graph)
 print(test_graph._patches_map[1].test())
+print("firefighters:", test_graph._firefighters_map)
+print("Data: ", test_graph._graph_data)
 
-@dataclass
-class Graphdata:
-    """Each instance of this class creates a dataclass that stores various information related to simulating the evolution of patches of land
-
-    Parameters
-    ----------
-    _land_patches: int, default = 0
-        Stores total number of landpatches on the graph (coresponds to total number of vertices)
-    _tree_patches: List[int], default = []
-        Stores current number of tree patches on the graph
-    _rock_patches: List[int], default = []
-        Stores current number of rock patches on the graph
-    _ignited_tree_patches: List[int], default = []
-        Stores current number of ignited tree patches
-    _consumed_tree_patches: int, default = 0
-        Stores total number of tree patches consumed by fire
-    _rock_to_tree_counter: int, default = 0
-        Stores number of rock patches that, during the course of simulation, have turned into tree patches
-    _firefighters: List[int] = []
-        Stores current number of firefighters
-    _dead_firefighters: int = 0
-        Stores number of firefighters, who have perished
-    """
-    _land_patches: int = 0
-    _tree_patches: List[int] = field(default_factory=list)
-    _rock_patches: List[int] = field(default_factory=list)
-    _ignited_tree_patches: List[int] = field(default_factory=list)
-    _consumed_tree_patches: int = 0
-    _rock_to_tree_counter: int = 0
-    _firefighters: List[int] = field(default_factory=list)
-    _dead_firefighters_counter: int = 0
-
-    def update_patches(self, patches_map: Dict[str, Type]) -> None:
-        """Updates number of tree patches, rock patches and forest fires"""
-
-        # Store variables for updating patches of instance
-        treepatches = self._tree_patches
-        rockpatches = self._rock_patches
-        forest_fires = self._ignited_tree_patches
-
-        # Initialize counters
-        treepatches_counter = 0
-        rockpatches_counter = 0
-        forest_fires_counter = 0
-
-        # Iterate over patches_map
-        for vertex, patch in patches_map.items():
-            # check if patch mapped to vertex is rock or tree
-            if isinstance(patch, lc.Rockpatch):
-                rockpatches_counter += 1
-            else:
-                treepatches_counter += 1
-                # check if tree patch is ignited
-                if patch.is_ignited:
-                    forest_fires_counter += 1
-        
-        # Append patch count to instance data
-        treepatches.append(treepatches_counter) 
-        rockpatches.append(rockpatches_counter)
-        forest_fires.append(forest_fires_counter) 
-
-    def update_rock_to_tree_counter(self) -> None:
-        """Updates number of rock patches that swapped to a tree patch"""
-        # TODO
-
-        self._rock_to_tree_counter += 1
-    
-    def update_firefighter_list(self) -> None:
-        """Updates the list of current alive fire fighters"""
-
-        # Compute number of currently alive firefighters
-        firefighters = self._firefighters[0] - self._dead_firefighters_counter
-
-        # Append total number of alive firefighters to firefighter list
-        self._firefighters.append(firefighters)
-
-    def update_dead_firefighters_counter(self):
-        """Updates counter of dead fire fighters"""
-        
-        self._dead_firefighters_counter += 1
 sys.exit()
 # function for generating af g
 def user_file(fp, fn):
