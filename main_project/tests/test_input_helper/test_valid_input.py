b78f6f99934fbfe7cb873010a59f0065876a272b
Chris Vandborg <chrisvandborg@MacBook-Pro-tilhrende-Chris.local>
2024-01-02 22:46:49 +0100

    added folder for tests. Should be working fine

---
+++ main_project/tests/test_input_helper/test_valid_input.py
@@ -0,0 +1,7 @@
+import sys
+import os
+
+# Add the path to the main_project folder to the Python path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'main_project')))
+
+from input_helper import valid_input
